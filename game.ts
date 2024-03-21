4307800b9c9f17f560a85bb670853463b7326448
nisarahmedbhutto <159589661+nisarahmedbhutto@users.noreply.github.com>
2024-03-21 05:42:20 +0500

    Add files via upload

---
+++ game.ts
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+function selectBox(): void {
+  console.log("start game level 1 are you ready");
+
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "selectedBox",
+        message: "select a one box:",
+        choices: [
+          { name: "green box", value: "option1" },
+          { name: "blue box", value: "option2" },
+          { name: "yellow box", value: "option3" },
+        ],
+      },
+    ])
+    .then((answer) => {
+      let check = Math.floor(Math.random() * 7) + 1;
+      if (check === 1) {
+        console.log("you win!");
+      } else if (check === 3) {
+        console.log("you saved!");
+      } else if (check === 7) {
+        console.log("good job!");
+      } else {
+        console.log("you lose!");
+      }
+    });
+}
+
+selectBox();
